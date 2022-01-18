acb3bafe644d00add41f9b0cea12cb51f22cd7dc
ghendersonnc <ghendersonnc@gmail.com>
2022-01-17 20:46:10 -0500

    test commit

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -1,15 +1,15 @@
 #pragma once
 
-
 #include <memory>
 
 #define BIND_EVENT(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
 namespace GH {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
-	template<typename T, typename ... Args>
-	constexpr Scope<T> CreateScope(Args&& ... args)
+	template<typename T, typename ... A>
+	constexpr Scope<T> CreateScope(A&& ... args)
 	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
+		return std::make_unique<T>(std::forward<A>(args)...);
 	}
 }
